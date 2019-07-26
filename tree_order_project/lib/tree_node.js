bbdc7ac4aadf50e5d1507965ddf1ffb660f737cd
aliglaser <nurinuriprettynuri@gmail.com>
2019-07-25 17:15:29 -0700

    treenode

--- tree_order_project/lib/tree_node.js
+++ tree_order_project/lib/tree_node.js
@@ -1,7 +1,10 @@
 class TreeNode {
-    
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
 }
 
 module.exports = {
-    TreeNode
+  TreeNode
 };
--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,13 +1,31 @@
 function inOrderArray(root) {
-    
+  let result = [];
+  if (!root) {
+    return [];
+  }
+
+  result = result.concat(inOrderArray(root.left));
+
+  result.push(root.val);
+  result = result.concat(inOrderArray(root.right));
+
+  return result;
 }
 
 function postOrderArray(root) {
+  let result = [];
+  if (!root) {
+    return [];
+  }
 
+  result = result.concat(postOrderArray(root.left));
+  result = result.concat(postOrderArray(root.right));
+  result.push(root.val);
+
+  return result;
 }
 
-
 module.exports = {
-    inOrderArray,
-    postOrderArray
+  inOrderArray,
+  postOrderArray
 };
