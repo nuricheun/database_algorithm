730d2ac0966592ae2d2385428068ccd411af53f5
aliglaser <nurinuriprettynuri@gmail.com>
2019-07-29 00:37:02 -0700

    practice maxvalue and regions

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,7 +1,15 @@
-function maxValue(node, visited=new Set()) {
+function maxValue(node, visited = new Set()) {
+  if (visited.has(node)) {
+    return -Infinity;
+  }
+  visited.add(node);
+  let maxes = node.neighbors.map(neighbor => {
+    return maxValue(neighbor, visited);
+  });
 
+  return Math.max(node.val, ...maxes);
 }
 
 module.exports = {
-    maxValue
+  maxValue
 };
--- graph_project/lib/num_regions.js
+++ graph_project/lib/num_regions.js
@@ -1,7 +1,29 @@
 function numRegions(graph) {
+  let visited = new Set();
+  let count = 0;
+  Object.keys(graph).forEach(node => {
+    if (visited.has(node)) {
+      return;
+    }
+    visited.add(node);
+    if (countEdge(graph, node, visited)) {
+      count++;
+    }
+  });
+  return count;
+}
 
+function countEdge(graph, node, visited) {
+  graph[node].forEach(e => {
+    if (visited.has(e)) {
+      return false;
+    }
+    visited.add(e);
+    countEdge(graph, e, visited);
+  });
+  return true;
 }
 
 module.exports = {
-    numRegions
+  numRegions
 };
