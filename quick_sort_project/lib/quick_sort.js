a7e77a6570a41ec2452e039fa81d1465c223b67c
aliglaser <nooti777712@gmail.com>
2019-08-10 17:23:01 -0700

    quicksort

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,8 +1,21 @@
 function quickSort(array) {
-
+  console.log(array);
+  if (array.length <= 1) return array;
+  let dividor = array[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > dividor) {
+      right.push(array[i]);
+    } else {
+      left.push(array[i]);
+    }
+  }
+  return quickSort(left)
+    .concat([dividor])
+    .concat(quickSort(right));
 }
 
-
 module.exports = {
-    quickSort
+  quickSort
 };
